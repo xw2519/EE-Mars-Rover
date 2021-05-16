991585554145c331623a49bf296579808722556b
Xin Wang <xw2519@ic.ac.uk>
2021-05-16 16:59:15 +0200

    html index created

--- src/server/server.py
+++ src/server/server.py
@@ -1,4 +1,8 @@
-""" This file defines how the server part of the backend functions """
+""" 
+    This file defines how the server part of the backend functions. 
+    
+    Upon execution, sets up a server on the specified IP and PORT defined in ".env" file.
+"""
 
 # Import server packages
 from os import environ
@@ -8,7 +12,7 @@ from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from socketio import *
 
-# Server core setting
+# Server core settings
 load_dotenv()
 HOST = environ.get("HOST")
 PORT = int(environ.get("PORT"))
