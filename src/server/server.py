fc3e91299a02d1ff2a761ee3a3b2ce60bac82c2c
Xin Wang <xw2519@ic.ac.uk>
2021-05-16 21:22:58 +0200

    Website serving mechanism completed

--- src/server/server.py
+++ src/server/server.py
@@ -11,15 +11,40 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from socketio import *
+from uvicorn import run
 
 # Server core settings
 load_dotenv()
 HOST = environ.get("HOST")
 PORT = int(environ.get("PORT"))
 URL = "ws://" + HOST + ":" + str(PORT)
-FAPI = FastAPI()
-Web_template = Jinja2Templates(directory="")
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+sio = AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+socket_app = ASGIApp(sio)
 
 
+@app.get("/")
+def root(request: Request):
+    """ Serves the rover command page """
+    return templates.TemplateResponse("index.html", {"request": request, "url": URL})
+
+"""
+Socket.IO event handlers
+
+@sio.on("connect")
+def connect(sid, environ):
+    print(f"Client connected, {sid}")
 
 
+@sio.on("disconnect")
+def disconnect(sid):
+    print(f"Client disconnected, {sid}")
+
+app.mount("/", socket_app)
+
+"""
+
+if __name__ == "__main__":
+    run('server:app', host=HOST, port=PORT)
