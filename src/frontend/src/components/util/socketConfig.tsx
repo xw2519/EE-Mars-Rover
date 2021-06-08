5495640ce2b04dbdd8e387ff069e1bf25becc3f6
Xin Wang <xw2519@ic.ac.uk>
2021-06-08 23:17:33 +0200

    Cleaned up unknown files

--- src/frontend/src/components/util/socketConfig.tsx
+++
@@ -1,7 +0,0 @@
-// Establish websocket connection with server
-
-//const ws_server = new WebSocket("ws://3.143.233.110:8000/ws/server"); 
-
-const ws_server = new WebSocket("ws://localhost:8000/ws/server"); 
-
-export default ws_server;
