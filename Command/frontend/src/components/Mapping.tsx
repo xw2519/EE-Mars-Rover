5dbdedf52a452f19775fe6728ce661328ab971e0
Xin Wang <xw2519@ic.ac.uk>
2021-06-09 21:21:39 +0200

    Direction animation completed

--- Command/frontend/src/components/Mapping.tsx
+++ Command/frontend/src/components/Mapping.tsx
@@ -12,6 +12,7 @@ interface CustomNode {
   y: number;
   color?: string;
   custom?: string;
+  angle?: number
 }
 
 interface CustomLink {
@@ -49,41 +50,48 @@ function LinkNodes(nodes: CustomNode[]) {
 
 export default function Map({ width, height}: NetworkProps) {
 
-  const circleRef = React.createRef<SVGCircleElement>();
+  function RoverNode ( angle ) {
 
-  const RoverNode = ( ) => (
+    var rotation = 'rotate( ' + angle.angle + ' 14 14 )'
 
+    console.log(rotation)
+
+    return (
       // Passes "angle" variable to determine the direction that the rover is facing
-
-      <svg width="40" height="40" fill="currentColor" className="bi-arrow-up-circle-fill" viewBox="0 0 24 24" transform='rotate(' + angle + '14 14)'>
+      <svg width="40" height="40" fill="currentColor" className="bi-arrow-up-circle-fill" viewBox="0 0 24 24" transform={rotation}>
         <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
       </svg>
+    )
 
-  );
+  }
 
-  const LocationNode = () => (
+  function LocationNode () {
 
-    <g transform={`translate(${-40 / 2}, ${-40 / 2})`}>
-      <svg className="icon-tabler-map-pin" width="30" height="30" viewBox="0 0 30 30" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      </svg>
-    </g>
+    return (
+      <g transform={`translate(${-40 / 2}, ${-40 / 2})`}>
+        <svg className="icon-tabler-map-pin" width="30" height="30" viewBox="0 0 30 30" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        </svg>
+      </g>
+    )
 
-  );
+  }
 
-  const ObstacleNode = ( color ) => (
-    <g transform={`translate(${-40 / 2},${-40 / 2})`}>
+  function ObstacleNode ( color ) {
 
-      <svg  xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-alert-octagon" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path 
-        d="M8.7 3h6.6c.3 0 .5 .1 .7 .3l4.7 4.7c.2 .2 .3 .4 .3 .7v6.6c0 .3 -.1 .5 -.3 .7l-4.7 4.7c-.2 .2 -.4 .3 -.7 .3h-6.6c-.3 0 -.5 -.1 -.7 -.3l-4.7 -4.7c-.2 -.2 -.3 -.4 -.3 -.7v-6.6c0 -.3 .1 -.5 .3 -.7l4.7 -4.7c.2 -.2 .4 -.3 .7 -.3z">
-      </path>
-      <line x1="12" y1="8" x2="12" y2="12"></line>
-      <line x1="12" y1="16" x2="12.01" y2="16"></line>
-      </svg>
-    </g>
+    return (
+      <g transform={`translate(${-40 / 2},${-40 / 2})`}>
+        <svg className="icon-tabler-alert-octagon" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill={color} stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path 
+          d="M8.7 3h6.6c.3 0 .5 .1 .7 .3l4.7 4.7c.2 .2 .3 .4 .3 .7v6.6c0 .3 -.1 .5 -.3 .7l-4.7 4.7c-.2 .2 -.4 .3 -.7 .3h-6.6c-.3 0 -.5 -.1 -.7 -.3l-4.7 -4.7c-.2 -.2 -.3 -.4 -.3 -.7v-6.6c0 -.3 .1 -.5 .3 -.7l4.7 -4.7c.2 -.2 .4 -.3 .7 -.3z">
+        </path>
+        <line x1="12" y1="8" x2="12" y2="12"></line>
+        <line x1="12" y1="16" x2="12.01" y2="16"></line>
+        </svg>
+      </g>
+    )
 
-  );
+  }
 
   const [nodes, set_nodes] = useState<CustomNode[]>([{x:0, y:0, custom:'Rover'}]);
 
@@ -143,9 +151,7 @@ export default function Map({ width, height}: NetworkProps) {
   }
   */
 
-  var links: CustomLink[] = [
-    
-  ];
+  var links: CustomLink[] = [];
 
   links = LinkNodes(nodes)
 
@@ -156,7 +162,6 @@ export default function Map({ width, height}: NetworkProps) {
 
   // Return SVG map
   return width < 10 ? null : (
-    <div>
 
     <svg width={width} height={height}>
       <rect width={width} height={height} rx={14} fill={background} />
@@ -166,8 +171,10 @@ export default function Map({ width, height}: NetworkProps) {
         top={350}
         left={425}
 
-        nodeComponent={({ node: { custom, color }  }) =>
-          (custom === 'Rover') ? <RoverNode /> : ((custom === 'Location') ? <LocationNode /> : <ObstacleNode color={color} />)
+        nodeComponent={({ node: { custom, color, angle }  }) =>
+          // RoverNode takes "angle" parameter to define direction of rover
+          // ObstacleNode takes "color" parameter to define the color of the obstacle 
+          (custom === 'Rover') ? <RoverNode angle={180}/> : ((custom === 'Location') ? <LocationNode /> : <ObstacleNode color={color} />)
         }
 
         linkComponent={({ link: { source, target } }) => (
@@ -184,8 +191,7 @@ export default function Map({ width, height}: NetworkProps) {
 
         )}
       />
-
     </svg>
-    </div>
+    
   );
 }
