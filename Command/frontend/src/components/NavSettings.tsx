bf83fa9dc94589f8dc7b198096311c58453a9b7f
Xin Wang <xw2519@ic.ac.uk>
2021-06-10 16:14:41 +0200

    Input range limitation

--- Command/frontend/src/components/NavSettings.tsx
+++ Command/frontend/src/components/NavSettings.tsx
@@ -1,42 +1,48 @@
-import {Helmet} from "react-helmet";
+var value
 
 const NavSettings = (NavSettings) => {
 
-    const handleDistChange = (event) => {
+  const handleDistChange = (event) => {
 
+    if(event >= 100) {
+      value = 99
+      NavSettings.setDist(99)
+    }
+    else {
       NavSettings.setDist(event)
-      
-    };
+    }
+    
+  };
 
-    return (
-    <div className="Rover_Settings"> 
-      <div className="Unit">
+  return (
+  <div className="Rover_Settings"> 
+    <div className="Unit">
 
-        <div className="Unit_title"> 
-            <span> Unit distance [cm] </span>
-        </div>
-
-        <div className="Unit_switch"> 
-            <label>
-              <input
-                  type = "number"
-                  placeholder="Unit"
-                  defaultValue="10"
-                  size={10}
-                  max={99}
-                  min={1}
-                  maxLength={3}
-                  className="Dist_value"
-                  onChange={e => handleDistChange(e.target.value)}
-
-              />
-            </label>
-        </div>
-            
+      <div className="Unit_title"> 
+          <span> Unit distance [cm] </span>
       </div>
 
+      <div className="Unit_switch"> 
+          <label>
+            <input
+                type = "number"
+                placeholder="Unit"
+                defaultValue="10"
+                size={10}
+                max={99}
+                min={1}
+                maxLength={3}
+                className="Dist_value"
+                onChange={e => handleDistChange(e.target.value)}
+
+            />
+          </label>
+      </div>
+          
     </div>
-    );
+
+  </div>
+  );
 };
 
 export default NavSettings
